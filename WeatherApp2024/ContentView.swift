2bbc67803c0a47502802fd42553c39f9c07c2cdc
ojel0001 <ojel0001@algonquinlive.com>
2024-12-08 02:47:47 -0500

    Initial Commit

---
+++ WeatherApp2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherApp2024
+//
+//  Created by sunny ojelabi on 2024-12-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApp2024/WeatherApp2024App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp2024App.swift
+//  WeatherApp2024
+//
+//  Created by sunny ojelabi on 2024-12-08.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
